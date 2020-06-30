61fc30f982270e274311cbfcc2639f687bf0c4b0
Phil Livermore <phil.livermore@outlook.com>
2020-06-30 14:09:40 +0100

    added build css

---
+++ build/precache-manifest.eecc91a5c01195a3730bbc8c517d5225.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546e96abeb2cfc01515bef9dc1bdc076",
+    "url": "/index.html"
+  },
+  {
+    "revision": "960a1c7c8b250eda8182",
+    "url": "/static/css/main.b150b712.chunk.css"
+  },
+  {
+    "revision": "fea7abc7eb7dc66675e9",
+    "url": "/static/js/2.2b8d302d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b8d302d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960a1c7c8b250eda8182",
+    "url": "/static/js/main.7d3d5c74.chunk.js"
+  },
+  {
+    "revision": "d8ab3765a9e3a06cdbc0",
+    "url": "/static/js/runtime-main.2804c549.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/static/media/bg-header-mobile.6b5ff5a0.svg"
+  }
+]);
